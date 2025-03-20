9f7f1f8b195743005844c2ad214983d474d271da
Ana Lins <anynha-lins@hotmail.com>
2025-03-20 01:32:04 -0300

    first commit

---
+++ src/test/java/dev/java10x/CadastroClientes/CadastroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
